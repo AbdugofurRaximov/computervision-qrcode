cb20db0974adf475b735f50b20c149b7bf40f846
AbdugofurRaximov <126331380+AbdugofurRaximov@users.noreply.github.com>
2023-12-12 11:00:32 +0500

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -14,17 +14,17 @@ while True:
     for barcode in decode(img):
         myData = barcode.data.decode('utf-8')
         print(myData)
-        # if myData in datalist:
-        #     output = "Kirish mumkin !!!!"
-        #     rang = (0, 255, 0)
-        # else:
-        #     output = "Kirish mumkin emas !!!!"
-        #     rang = (0, 0, 255)
+        if myData in datalist:
+            output = "Kirish mumkin !!!!"
+            rang = (0, 255, 0)
+        else:
+            output = "Kirish mumkin emas !!!!"
+            rang = (0, 0, 255)
         pts = np.array([barcode.polygon], np.int32)
         pts = pts.reshape((-1, 1, 2))
         cv2.polylines(img, [pts], True, (255,0,255), 5)
         pts2 = barcode.rect
-        cv2.putText(img, myData, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, myData, 2)
+        cv2.putText(img, output, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, rang, 2)
     cv2.imshow("Natija", img)
     if cv2.waitKey(1) == ord("q"):
         break
